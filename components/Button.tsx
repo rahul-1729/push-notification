14348aa87bd602920c146832c3d29b5bb806d7d5
rahul-1729 <rk9763981@gmail.com>
2024-07-29 17:03:02 +0530

    completed as asked

---
+++ components/Button.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+export default function Button()
+{  
+    const handleClick= async()=>{
+        const permission = await Notification.requestPermission();
+
+        if (permission === 'granted') {
+             
+            new Notification('Button: Hey you,stop clicking me 😂', {
+                body: 'You have a push notification',
+                icon: '/logo.jpg',
+                 
+            });
+        } else {
+            console.error('Notification permission denied');
+        }
+    }
+    return(
+        <button onClick ={handleClick}>Send Notification</button>
+    )
+    
+}
